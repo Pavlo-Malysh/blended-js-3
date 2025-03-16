9320a600e6933bd3fd4d0ed243641739d73d40dd
Pavlo Malysh <mal.pawlo@gmail.com>
2025-03-16 21:30:00 +0200

    update file

--- js/task-blended-01.js
+++ js/task-blended-01.js
@@ -1,22 +1,4 @@
-
-// console.log(Boolean("false"));
-
-// const r = 56;
-
-// if (r > 50 && r < 70) {
-//     console.log("Yes, its right result!");
-// }
-
-// else {
-//     console.log("false");
-// }
-
-// console.log("hello", 5);
-
-// const der = "Hello world!";
-
-// console.log(der.length);
-
+// Додаткові завдання blended 15.03
 
 
 // Task 1
@@ -51,7 +33,6 @@ function getQuarter() {
     }
 
 }
-
 // const UserNumber = getQuarter();
 
 
@@ -65,7 +46,6 @@ function getQuarter() {
 // введе в prompt щось інше. В такому випадку result має набувати значення:
 // "Вибачте, але ви маєте ввести значення від 1 до 4 включно".
 // Значення змінної result виведіть в консоль.
-
 
 
 function getNum() {
@@ -90,10 +70,10 @@ function getNum() {
     return result;
 }
 
-console.log(getNum());
+// console.log(getNum());
 
 
-
+// Task 3
 // Отримуйте від користувача число (кількість хвилин) через prompt
 // і виводьте у консоль рядок у форматі годин та хвилин.
 // Приклад: користувач вводить в prompt '70' -> в консолі відобразиться '01:10'.
@@ -101,30 +81,27 @@ console.log(getNum());
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart#examples
 
 
-// ПЕРЕРОБИТИ
-// function getNumber() {
-//     let num = +prompt("Введіть кількість хвилин")
-//     let time;
 
-//     let hours = num - num % 60;
-//     let min = num % 60;
-//     console.log(hours, min)
-//     if (hours >= 60) {
-//         time = hours / 60;
-//     }
-//     time
+function getNumber() {
+    let getTime = prompt("Введіть кількість хвилин")
+    getTime = Number(getTime)
+    let hours = Math.floor(getTime / 60);
+    let minutes = getTime % 60;
 
-// }
+    printHours = String(hours).padStart(2, "0")
+    printMinutes = String(minutes).padStart(2, "0")
 
-// console.log((70 - (70 % 60)))
-// console.log(150 / 60)
+    return `${printHours}:${printMinutes}`
+
+
+}
 
 // console.log(getNumber())
 
 
 
 
-
+// Task 4
 // Напишіть код, який запитуватиме у користувача
 // логін за допомогою prompt і виводить результат в консоль браузера
 
@@ -140,30 +117,25 @@ console.log(getNum());
 // в іншому випадку вивести в alert рядок "Невірний пароль!"
 
 
-// function getLogin() {
-//     let login = prompt("Введіть логін")
+function getLogin() {
+    let login = prompt("Введіть логін")
+    console.log(login);
 
+    if (login.toLowerCase() === "адмін") {
+        let userPassword = prompt("Введіть пароль")
+        if (userPassword === null) {
+            alert("Скасовано")
+        } else if (userPassword === "Я головний") {
+            alert("Добрий день!")
+        } else {
+            alert("Невірний пароль!")
+        }
+    }
+}
 
-//     if (login === "Адмін") {
-//         password = prompt("Введіть пароль")
-//         let login = prompt("Введіть логін")
+// console.log(getLogin());
 
-//     }
-//     else if (login === "") {
-//         alert("Скасовано")
-//     }
-//     else {
-//         alert("Я вас не знаю")
-//     };
-
-//     if (password === "Я головний") {
-//         alert("Добрий день!")
-//     } else {
-//         alert("Невірний пароль!")
-//     }
-// }
-
-
+// Task 5
 // Напишіть функцію getNumbers(min, max), що приймає 2 параметри - 
 // мінімальне і максимальне число відповідно.
 // Напишіть цикл всередині функції, який виводить у консоль
@@ -172,15 +144,21 @@ console.log(getNum());
 // і поверніть її з функції.
 
 
-// getNumbers(min, max) {
-//     let counter;
-//     for (counter = 0; max >= min; counter++) {
+function getNumbers(min, max) {
+    let sum = 0;
+    for (i = max; i >= min; i--) {
+        console.log(i);
+        if (i % 2 === 0) {
+            sum += i
+        }
+    }
+    return sum
+}
 
-//     }
-// }
+// console.log(getNumbers(1, 20));
 
 
-
+// Task 6
 // Напишіть функцію min(a, b), яка приймає 2 числа і повертає меньше з них.
 // Потрібно додати перевірку, що функція отримує саме числа, в іншому випадку
 // повертати з функції рядок - 'Not a number!'.
@@ -199,9 +177,11 @@ function min(a, b) {
 
 }
 
-console.log(min(90, 9))
+// console.log(min(90, 9))
 
 
+
+// Task 7
 // Напишіть функцію isAdult(age), яка  приймає число - вік користувача і повертає true, 
 // якщо параметр age більше чи дорівнює 18.
 // В іншому випадку вона запитує підтвердження через confirm 
@@ -215,4 +195,62 @@ function isAdult(age) {
     }
 }
 
-console.log(isAdult(45))
+// console.log(isAdult(45))
+
+
+// Task 8
+
+// Запропонуйте користувачу ввести число за допомогою prompt().
+// Збережіть введене значення у змінну number.
+// Перевірте, чи дорівнює змінна number числу 10:
+//    Якщо так – виведіть повідомлення 'Вірно' у alert().
+//    Якщо ні – виведіть 'Невірно'. 
+
+// Підказка: prompt завжди повертає рядок (String), тому перед перевіркою  
+// перетворіть значення на число за допомогою Number().
+
+
+function getNumbersUser() {
+
+    let number = prompt("Введіть довільне число")
+    if (Number(number) === 10) {
+        alert("Вірно")
+    } else {
+        alert("Невірно")
+    }
+}
+
+// console.log(getNumbersUser())
+
+
+
+// Task 9
+
+// Використайте цикл while, щоб вивести в console всі числа від 0 до 20 включно.
+// let i = 1;
+// while (i <= 20) {
+//     console.log(i);
+//     i++
+// }
+
+
+// Task 10
+// Напишіть функцію fizzBuzz(num), яка приймає число і перевіряє кожне число від 1 до num:
+// Якщо число ділитися  без остачі на 3 - виводить в консоль 'fizz',
+// якщо ділиться  без остачі на 5 - виводить в консоль 'buzz',
+// якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль 'fizzbuzz'.
+
+
+function fizzBuzz(num) {
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log(`${i} fizzbuzz`);
+        } else if (i % 5 === 0) {
+            console.log(`${i} buzz`);
+        } else if (i % 3 === 0) {
+            console.log(`${i} fizz`);
+        }
+    }
+}
+
+// console.log(fizzBuzz(15));
---
+++ js/task-module-02.js
@@ -0,0 +1,141 @@
+// Завдання з модуля 2 автоперевірки. Розгалуження та цикли
+
+// Task 1
+// Функція getFileName(file) приймає один параметр
+
+// file - рядок з іменем файлу.Ім'я файлу може бути з розширенням, наприклад: styles.css, app.js або без, наприклад: styles, app.
+// Використовуючи методи indexOf та slice, доповни код функції таким чином, щоб:
+
+// Вона перевіряла наявність розширення в імені файлу(назва розширення відокремлюється від імені файла крапкою)
+// Якщо ім'я файлу не містить розширення, то функція повертала новий підрядок, що містить ім'я файлу без змін
+// В іншому разі функція повертала підрядок з іменем файлу, але без розширення
+
+
+function getFileName(file) {
+    const indexFile = file.indexOf(".")
+    // const expFile = file.slice(indexFile)
+    if (indexFile < 0) {
+        return file.slice(0);
+    } else {
+        return file.slice(0, indexFile);
+    }
+}
+
+// console.log(getFileName("styles.css"))
+
+
+
+// Task 2
+// Функція createFileName(name, ext) приймає два параметри:
+
+// name - рядок, що зберігає ім'я файлу без розширення, яке вводить користувач. Воно може містити зайві пробіли на початку або в кінці рядка, наприклад "order ", " finance " тощо
+// ext - рядок, що зберігає розширення, наприклад "txt", "xml" тощо
+// Використовуючи синтаксис шаблонних рядків і метод trim(), доповни код функції таким чином, щоб вона повертала повне(об'єднане) ім'я файлу з доданим розширенням, зазначеним у параметрі ext у форматі ім'я.розширення. Також повне ім'я файлу не повинно містити зайвих пробілів на початку або наприкінці.
+
+
+function createFileName(name, ext) {
+    name = name.trim()
+    newName = name + "." + ext
+    newName = newName.trim()
+    return newName
+}
+
+// console.log(createFileName("user   ", "txt"))
+
+
+function createFileName(name, ext) {
+    return `${name.trim()}.${ext.trim()}`
+}
+
+// console.log(createFileName("   user ", "   js   "))
+
+
+
+
+// Task 3
+// Функція calculateTotal(number) приймає ціле число(параметр number).Доповни код функції так, щоб вона повертала суму всіх цілих чисел від одиниці до цього числа включно.Наприклад, якщо number дорівнює 3, то сума - це 1 + 2 + 3, тобто 6.
+
+
+
+function calculateTotalWhile(number) {
+    let sum = 0
+    let i = 1
+    while (i < number) {
+        sum = sum + i
+        i++
+
+    }
+    return sum + number
+}
+
+
+// console.log(calculateTotalWhile(18))
+
+function calculateTotalFor(number) {
+    let sum = 0
+    for (let i = 0; i <= number; i++) {
+        sum += i
+
+    }
+    return sum
+}
+
+// console.log(calculateTotalFor(18))
+
+
+
+
+// Task 4
+// Функція calculateEvenTotal(number) приймає ціле число(параметр number).Доповни код функції так, щоб вона повертала суму всіх парних цілих чисел від одиниці до цього числа включно.Парні числа — це ті, що можна поділити на 2 без остачі(як це зробити розглядалося у темі Арифметичні операції у модулі 1).Наприклад, якщо number дорівнює 6, то сума - це 2 + 4 + 6, тобто 12.
+
+
+function calculateEvenTotal(number) {
+    let sum = 0
+
+    for (let i = 0; i < number; i += 2) {
+        sum += i
+    }
+    if (number % 2 === 0) {
+        sum = sum + number
+    }
+
+    return sum
+}
+
+
+// console.log(calculateEvenTotal(27))
+
+
+// Task 5
+// Доповни код таким чином, щоб у змінну number записувалося перше число у проміжку від start до end, яке ділиться на 5 без остачі.
+
+const start = 6;
+const end = 17;
+let number;
+
+for (number = start; number <= end; number++) {
+    if (number % 5 === 0) {
+        break;
+    }
+}
+
+// Task 6
+// Функція findNumber(start, end, divisor) приймає три параметра, які є цілими числами.
+
+// Доповни код функції таким чином, щоб вона:
+
+// повертала перше число у діапазоні від start до end включно, яке ділиться на divisor без остачі
+// не використовуй оператор break
+// findNumber(2, 6, 5) повертає 5
+// Виклик findNumber(8, 17, 3) повертає 9
+
+
+function findNumber(start, end, divisor) {
+    let number;
+    for (number = start; number <= end; number++) {
+        if (number % divisor === 0) {
+            return number
+        }
+    }
+}
+
